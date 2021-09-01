697f98821d2f3c017bf183e860b592bc141ac1a7
Salim Semakula <salimsema@yahoo.com>
2021-09-01 10:17:08 +0300

    Speciality

--- src/components/Speciality.js
+++ src/components/Speciality.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import cardio from '../images/heart.png'
 import kidney from '../images/kidneys.png'
 import lung from '../images/ribbon.png'
@@ -13,14 +15,18 @@ import child from '../images/heartc.png'
 import bone from '../images/bone.png'
 
 const Wrap = styled(Container)`
-  display: flex;
-  justify-content: center;
-  flex-wrap: no-wrap;
   cursor: pointer;
+  width: 70vw;
 
   @media only screen and (max-width: 600px) {
     width: 100vw;
-    flex-wrap: wrap;
+  }
+`
+const ImgBox = styled.div`
+  transition: 0.8s ease-in-out;
+  &:hover {
+    transform: translateY(3px);
+    color: #003638;
   }
 `
 
@@ -29,114 +35,116 @@ export default function Speciality() {
     <>
       <div className="py-5 text-muted">
         <h2 className="text-center">KEY SPECIALITIES</h2>
-        <Wrap fluid>
-          <section className="d-flex">
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={cardio}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Cardiac Surgery</p>
-            </div>
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={kidney}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Kidney Transplant</p>
-            </div>
-          </section>
-
-          <section className="d-flex">
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={lung}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Cancer Surgery</p>
-            </div>
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={ortho}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Joint Replacement</p>
-            </div>
-          </section>
-        </Wrap>
 
         <Wrap>
-          <section className="d-flex">
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={spine}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Neuro & Spine Surgery</p>
-            </div>
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={liver}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Liver Transplant</p>
-            </div>
-          </section>
+          <Row>
+            <Col md={6}>
+              <section className="d-flex">
+                <div className="text-center  font-weight-bold p-3 ">
+                  <ImgBox>
+                    <p>
+                      <img src={cardio} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Cardiac Surgery</p>
+                  </ImgBox>
+                </div>
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={kidney} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Kidney Transplant</p>
+                  </ImgBox>
+                </div>
+              </section>
+            </Col>
+            <Col md={6}>
+              <section className="d-flex">
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={lung} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Cancer Surgery</p>
+                  </ImgBox>
+                </div>
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={ortho} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Joint Replacement</p>
+                  </ImgBox>
+                </div>
+              </section>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <section className="d-flex">
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={spine} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Neuro & Spine Surgery</p>
+                  </ImgBox>
+                </div>
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={liver} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Liver Transplant</p>
+                  </ImgBox>
+                </div>
+              </section>
+            </Col>
+            <Col md={6}>
+              <section className="d-flex">
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={fat} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Bariatric Surgery</p>
+                  </ImgBox>
+                </div>
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={radio} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Radiation Therapy</p>
+                  </ImgBox>
+                </div>
+              </section>
+            </Col>
+          </Row>
 
-          <section className="d-flex">
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={fat}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Bariatric Surgery</p>
-            </div>
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={radio}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '200px' }}
-              />
-              <p>Radiation Therapy</p>
-            </div>
-          </section>
-        </Wrap>
-        <Wrap>
-          <section className="d-flex">
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={child}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '100px' }}
-              />
-              <p>Paediatric Cardiology</p>
-            </div>
-            <div className="text-center p-3 font-weight-bold">
-              <img
-                src={bone}
-                alt="speciality"
-                className="w-100"
-                style={{ height: '100px', width: '100px' }}
-              />
-              <p>Bone Marrow Transplant</p>
-            </div>
-          </section>
+          <Row>
+            <Col md={6}>
+              <section className="d-flex">
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={child} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Paediatric Cardiology</p>
+                  </ImgBox>
+                </div>
+                <div className="text-center  font-weight-bold p-3">
+                  <ImgBox>
+                    <p>
+                      <img src={bone} alt="speciality" className="w-100" />
+                    </p>
+                    <p>Bone Marrow Transplant</p>
+                  </ImgBox>
+                </div>
+              </section>
+            </Col>
+            <Col md={6}></Col>
+          </Row>
         </Wrap>
       </div>
     </>
